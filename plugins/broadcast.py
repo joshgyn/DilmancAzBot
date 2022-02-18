89a787c0b07e98b8e73633680b2a417ac12bcac8
Je Luxe <78498193+joshgyn@users.noreply.github.com>
2022-02-19 02:01:02 +0400

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -7,10 +7,10 @@ ADMIN = int(os.environ.get("ADMIN", 923943045))
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
-   ms = await message.reply_text("Geting All ids from database ...........")
+   ms = await message.reply_text("Bütün istifadəçilər data mərkəzindən çağırılır····>")
    ids = getid()
    tot = len(ids)
-   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   await ms.edit(f"Yayım başlanır ····> \n  {tot} İstifadəçiyə mesaj göndərilir ⏳")
    for id in ids:
      try:
      	await message.reply_to_message.copy(id)
